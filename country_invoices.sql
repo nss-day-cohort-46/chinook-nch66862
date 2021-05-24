d340a41e35056646f34264fdc685261a272b89ad
Nick Carver <nicholascarver2@outlook.com>
2021-05-24 14:27:30 -0500

    14

--- country_invoices.sql
+++ country_invoices.sql
@@ -1,15 +1,5 @@
 SELECT 
-    il.InvoiceLineId,
-    il.InvoiceId,
-    il.TrackId,
-    il.UnitPrice,
-    il.Quantity,
-    t.Name,
-    ar.Name as ArtistName
-FROM InvoiceLine il
-INNER JOIN Track t ON
-    il.TrackId = t.TrackId
-INNER JOIN Album a ON
-    a.AlbumId = t.AlbumId
-INNER JOIN Artist ar ON
-    ar.ArtistId = a.ArtistId
+    i.BillingCountry,
+    COUNT(*) as NumberOfInvoices
+FROM Invoice i
+GROUP BY i.BillingCountry
