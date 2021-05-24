74826aca738d0217ece26411f7f0bbfee1b69b62
Nick Carver <nicholascarver2@outlook.com>
2021-05-24 14:00:16 -0500

    13

--- line_item_track.sql
+++ line_item_track.sql
@@ -4,7 +4,12 @@ SELECT
     il.TrackId,
     il.UnitPrice,
     il.Quantity,
-    t.Name
+    t.Name,
+    ar.Name as ArtistName
 FROM InvoiceLine il
 INNER JOIN Track t ON
     il.TrackId = t.TrackId
+INNER JOIN Album a ON
+    a.AlbumId = t.AlbumId
+INNER JOIN Artist ar ON
+    ar.ArtistId = a.ArtistId
