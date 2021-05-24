c227a42c2c531396552fccd81c349f564a048a5b
Nick Carver <nicholascarver2@outlook.com>
2021-05-24 14:23:03 -0500

    did fancy date thing

--- total_sales_2009.sql
+++ total_sales_2009.sql
@@ -1,4 +1,6 @@
 SELECT 
+    strftime("%Y", i.InvoiceDate) AS InvoiceYear,
     SUM(i.Total) AS TotalSales
 FROM Invoice i
-WHERE InvoiceDate LIKE "2009%"
+WHERE InvoiceYear = "2009" OR InvoiceYear = "2011"
+GROUP BY InvoiceYear
